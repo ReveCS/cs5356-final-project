46e7b73d67708eb734d268a3f8930b8961f45d86
Avery Chan <realac777@gmail.com>
2025-05-05 19:04:48 -0400

    fixing vercel errors

--- src/components/profile/PlaceList.tsx
+++ src/components/profile/PlaceList.tsx
@@ -162,7 +162,7 @@ export default function PlaceList({ wantToTryPlaces, savedPlaces, isLoading, err
                     variant="ghost"
                     size="icon"
                     className="absolute top-2 right-2 opacity-0 group-hover:opacity-100 transition-opacity"
-                    onClick={() => handleDelete(place.place_id)}
+                    onClick={() => handleDelete(place.place_id!)}
                     disabled={isDeleting === place.place_id}
                   >
                     <Trash2 className="h-4 w-4 text-red-500" />
